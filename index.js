b0d4b721d15fedc8ad380029289121221a2dc194
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-01 17:15:32 +0200

    Fix indentation for better readability

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(express.json());
 app.use(require('cors')());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Remote Work!');
+    res.send('Welcome to Remote Work!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
