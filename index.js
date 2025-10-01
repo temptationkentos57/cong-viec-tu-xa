2812184b62475b82a72c16133922ef7425248f69
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-01 17:14:49 +0200

    Update welcome message to English for better accessibility.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(express.json());
 app.use(require('cors')());
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Công Việc Từ Xa!');
+  res.send('Welcome to Remote Work!');
 });
 
 app.listen(port, () => {
-  console.log(`Server đang chạy trên http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
